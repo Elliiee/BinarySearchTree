ce9e9e266ebdbec650fbbd10512662820a6ee3f8
Elliiee <danyiluozhang@gmail.com>
2020-02-17 15:59:28 -0600

    validate BST iteration

---
+++ IntroductionBST/src/ValidateBSTIteration.java
@@ -0,0 +1,34 @@
+import java.util.LinkedList;
+
+public class ValidateBSTIteration {
+    LinkedList<TreeNode> stack = new LinkedList<>();
+    LinkedList<Integer> uppers = new LinkedList<>();
+    LinkedList<Integer> lowers = new LinkedList<>();
+
+    public void update(TreeNode root, Integer lower, Integer upper){
+        stack.add(root);
+        lowers.add(lower);
+        uppers.add(upper);
+    }
+
+    public boolean isValidBST(TreeNode root){
+        Integer lower = null, upper = null, val;
+        update(root, lower, upper);
+
+        while (!stack.isEmpty()){
+            root = stack.poll();
+            lower = lowers.poll();
+            upper = uppers.poll();
+
+            if (root == null) continue;
+
+            val = root.val;
+            if (lower != null && val <= lower) return false;
+            if (upper != null && val >= upper) return false;
+
+            update(root.right, val, upper);
+            update(root.left, lower, val);
+        }
+        return true;
+    }
+}
