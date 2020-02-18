4bf1f2a34c372085c28198fe11fa4fee5f131ac0
Elliiee <danyiluozhang@gmail.com>
2020-02-18 13:48:27 -0600

    Search BST iteration

---
+++ BasicOperation/src/SearchBSTIteration.java
@@ -0,0 +1,12 @@
+public class SearchBSTIteration {
+    public TreeNode searchBST(TreeNode root, int target){
+        TreeNode cur = root;
+        while (cur != null && cur.val != target){
+            if (target < cur.val)
+                cur = cur.left;
+            else
+                cur = cur.right;
+        }
+        return cur;
+    }
+}
